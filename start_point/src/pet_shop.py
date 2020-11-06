cc8f1567e3e3b7bdc5e3dd6ecf661f916b774955
Mark Richardson <mark_richardson@sky.com>
2020-11-06 17:56:38 +0000

    Updating comments to make clearer and add next test passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,30 +4,31 @@
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
 
-# return total cash
+# return total anount of cash
 def get_total_cash(pet_shop):
     return pet_shop ["admin"]["total_cash"]
 
-# add cash or remove cash
+# add cash or remove cash (remember 'adding' negative amount of cash will remove it)
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] +=  cash
 
-# get number of pets sold
+# get the total number of pets sold
 def get_pets_sold(pets_sold):
     return pets_sold ["admin"]["pets_sold"]
 
-# add to the number of pets sold
+# add to the total number of pets sold
 def increase_pets_sold (new_pet_sold, pets_sold):
     new_pet_sold ["admin"]["pets_sold"] += pets_sold
 
-# doing a count of stock (how many pets)
+# doing a count of pets in stock
 def get_stock_count(pets_in_shop):
     return len(pets_in_shop["pets"])
 
-# adding a function to return pets by their breed (when breed is found)
+# return pets by their breed, when the breed is found (returns count) or not found (returns 0)
 def get_pets_by_breed(pet_shop, pet_breed):
     num_pets = []
     for pet in pet_shop["pets"]:
         if pet["breed"] == pet_breed:
             num_pets.append(pet)
     return num_pets
+
