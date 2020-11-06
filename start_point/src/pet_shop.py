87ea4303667db8fc844fdac70f05eefc90d9f60f
Mark Richardson <mark_richardson@sky.com>
2020-11-06 16:32:34 +0000

    Adding function to return total cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,5 +1,10 @@
 # WRITE YOUR FUNCTIONS HERE
 
+# return the name of the shop
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
+
+# return total cash
+def get_total_cash(petshop_cash):
+    return petshop_cash ["admin"]["total_cash"]
 
